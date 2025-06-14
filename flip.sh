1b1bcb7e47b976164a371e27530db83cf9bca000
jgolden5 <jonathan.golden.gurr@gmail.com>
2025-06-13 22:13:41 -0700

    multiple inputs can be passed as paramters for Flip IN ORDER

--- flip.sh
+++ flip.sh
@@ -1,10 +1,51 @@
 #!/bin/bash
 
-full_prompt=""
+prompt=""
 ai="${ai:-$clipboard}"
 
-if [[ $1 =~ ^- ]]; then
-  echo "First flip parameter \"$1\" WAS a flag"
-else
-  echo "First flip parameter \"$1\" was NOT a flag"
-fi
+flip() {
+  if [[ $1 =~ ^- ]]; then
+    ops="${1:1}"
+    shift
+    prompt="$@"
+    execute_ops $ops
+  else
+    echo "First flip parameter \"$1\" was NOT a flag"
+  fi
+  echo "Flip Prompt = $prompt"
+}
+
+execute_ops() {
+  first_op="${1:0:1}"
+  execute_op $first_op
+  remaining_ops="${1:1}"
+  if [[ $remaining_ops ]]; then
+    execute_ops $remaining_ops
+  fi
+}
+
+execute_op() {
+  op="$1"
+  if [[ ${#op[@]} -gt 1 ]]; then
+    echo "Op is too long"
+    return 1
+  else
+    case $op in
+      a)
+        prompt="$prompt A"
+        ;;
+      b)
+        prompt="B + $prompt"
+        ;;
+      c)
+        prompt="C = $prompt"
+        ;;
+      *)
+        echo "Op not recognized"
+        ;;
+    esac
+  fi
+}
+
+#expected output for test: B + C = Test A
+#to get this output, user must run 'flip -acb Test'
